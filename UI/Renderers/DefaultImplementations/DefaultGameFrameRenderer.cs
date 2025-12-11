e8697e00bcf5de59c7a1cb19cfcc7bdcfb535fec
Beezi AI <ai-bot@beezi.io>
2025-12-11 14:51:17 +0000

    feat: Change Snake Color To Purple
    SCRUM-131

--- UI/Renderers/DefaultImplementations/DefaultGameFrameRenderer.cs
+++ UI/Renderers/DefaultImplementations/DefaultGameFrameRenderer.cs
@@ -54,8 +54,8 @@ namespace SnakeGame.UI.Renderers.DefaultImplementations
 
                     Console.ForegroundColor = curr switch
                     {
-                        "*" => ConsoleColor.DarkMagenta,
-                        "@" => ConsoleColor.Magenta,
+                        "*" => ConsoleColor.DarkRed,
+                        "@" => ConsoleColor.Red,
                         "●" => ConsoleColor.Red,
                         _ => ConsoleColor.White
                     };
